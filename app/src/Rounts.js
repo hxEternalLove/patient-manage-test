85894480355559dde5d4f6035dbf24e15ab654b1
zhuanghui <zhuanghui@jeejen.com>
2017-10-19 15:54:37 +0800

    增加 自定义字体

--- app/src/Rounts.js
+++ app/src/Rounts.js
@@ -1,23 +1,39 @@
+import React from 'react';
 import { StackNavigator, TabNavigator, TabBarBottom } from 'react-navigation';
 
 import NotificationScreen from './views/TabNotification.template';
 import PatientManageScreen from './views/TabPatientManage.template';
 import PersonalScreen from './views/TabPersonal.template';
-// import IconFont from './sources/IconFont'
+import IconFont from './resources/fonts/IconFont'
 
 const NotifcationNav = StackNavigator({
 	Home: {
 		screen: NotificationScreen,
 		navigationOptions: ({navigation}) => ({
 			headerTitle: '医医',
+			headerTitleStyle:{
+				color:'white',
+				fontSize:20
+			},
+			headerStyle:{
+				backgroundColor:'#3db1b2',
+			},
 		}),
 	}
+
 });
 const PatientManageNav = StackNavigator({
 	Home: {
 		screen: PatientManageScreen,
 		navigationOptions: ({navigation}) => ({
 			headerTitle: '🌺🌺',
+			headerTitleStyle:{
+				color:'white',
+				fontSize:20
+			},
+			headerStyle:{
+				backgroundColor:'#3db1b2'
+			}
 		}),
 	}
 });
@@ -25,7 +41,14 @@ const PersonalNav = StackNavigator({
 	Home: {
 		screen: PersonalScreen,
 		navigationOptions: ({navigation}) => ({
-			title: '我的',
+			headerTitle: '工作站',
+			headerTitleStyle:{
+				color:'white',
+				fontSize:20
+			},
+			headerStyle:{
+				backgroundColor:'#3db1b2'
+			}
 		}),
 	}
 });
@@ -35,16 +58,16 @@ const MyApp = TabNavigator({
 		navigationOptions: ({navigation}) => ({
 			tabBarLabel: '医医',
 			title: '医医',
-			// tabBarIcon: (
-			// 	<IconFont name={'icon_home'} style={{fontSize: 22}}/>)
+			tabBarIcon: ({focused, tintColor}) =>(
+				<IconFont name={'icon_home'} style={{fontSize: 22,color:tintColor}}/>)
 		}),
 	},
 	PatientManage: {
 		screen: PatientManageNav,
 		navigationOptions: ({navigation}) => ({
 			tabBarLabel: '患者管理',
-			// tabBarIcon: (
-			// 	<IconFont name={'icon_patient_manage'} style={{fontSize: 22}}/>)
+			tabBarIcon: ({focused, tintColor}) => (
+				<IconFont name={'icon_patient_manage'} style={{fontSize: 22,color:tintColor}}/>)
 		}),
 	},
 	Personal: {
@@ -52,8 +75,8 @@ const MyApp = TabNavigator({
 		navigationOptions: ({navigation}) => ({
 			tabBarLabel: '工作站',
 			title: '工作站',
-			// tabBarIcon: (
-			// 	<IconFont name={'icon_workspace'} style={{fontSize: 22}}/>)
+			tabBarIcon: ({focused, tintColor}) => (
+				<IconFont name={'icon_workspace'} style={{fontSize: 22, color:tintColor}}/>)
 		}),
 	},
 }, {
@@ -63,7 +86,7 @@ const MyApp = TabNavigator({
 	animationEnabled: false,// 是否在更改标签时显示动画
 	lazy: true,// 是否根据需要懒惰呈现标签，而不是提前，意思是在app打开的时候将底部标签栏全部加载，默认false,推荐为true
 	tabBarOptions: {// 配置标签栏的一些属性iOS属性
-		activeTintColor: '#6eb86a',// label和icon的前景色 活跃状态下
+		activeTintColor: '#3db1b2',// label和icon的前景色 活跃状态下
 		inactiveTintColor: '#979797',// label和icon的前景色 不活跃状态下
 		style: {backgroundColor: '#ffffff'},
 		labelStyle: {// label的样式安卓属性
